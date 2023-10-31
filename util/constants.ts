30e8440d571231f3be39a7620092bfabfc4ef3f2
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-11-01 00:30:47 +0600

    add: variables

--- util/constants.ts
+++ util/constants.ts
@@ -2,11 +2,17 @@ import { THomeSettingSectionNames } from "@/types/others";
 
 export const MAX_FILE_SIZE_LIMIT = 300000; // 300 kb
 
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "https://gari-chai.onrender.com";
 
 export const API_V1_URL = API_BASE_URL + "/api/v1";
 
-export const UI_BASE_URL = "http://localhost:3000";
+export const UI_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://gari-chai.vercel.app";
 
 export const PRIMARY_COLOR = "#3C50E0";
 
