d0a784595ae1fa8be893ee8719229cd0c11fab51
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-11-04 03:52:33 +0600

    update: car schema

--- services/cars/getCars.ts
+++ services/cars/getCars.ts
@@ -5,44 +5,51 @@ import { ReqMethod, TAGS, endpoints } from "..";
 export const carSchema = z.object({
   _id: z.string(),
   name: z.string(),
+  slug: z.string(),
   year: z.number(),
   registrationYear: z.number(),
-  description: z.string(),
+  description: z.string().optional(),
   brand: z.object({ slug: z.string(), name: z.string() }),
   modelNumber: z.number(),
   engine: z.object({
     type: z.string(),
-    displacement: z.number(),
-    horsePower: z.number(),
-    torque: z.number(),
+    displacement: z.number().optional(),
+    horsePower: z.number().optional(),
+    torque: z.number().optional(),
   }),
   transmission: z.string(),
   bodyStyle: z.string(),
   fuel: z.object({
     type: z.string(),
-    economy: z.object({ city: z.number(), highway: z.number() }),
+    economy: z
+      .object({ city: z.number().optional(), highway: z.number().optional() })
+      .optional(),
   }),
-  acceleration: z.object({ zeroTo60: z.number(), topSpeed: z.number() }),
-  safetyFeatures: z.string(),
-  infotainmentSystem: z.string(),
+  acceleration: z
+    .object({
+      zeroTo60: z.number().optional(),
+      topSpeed: z.number().optional(),
+    })
+    .optional(),
+  safetyFeatures: z.string().optional(),
+  infotainmentSystem: z.string().optional(),
   mileage: z.number(),
   posterImage: z.object({
-    originalUrl: z.string(),
-    thumbnailUrl: z.string(),
-    _id: z.string(),
+    originalUrl: z.string().url(),
+    thumbnailUrl: z.string().url(),
   }),
-  imageUrls: z.array(z.unknown()),
+  imageUrls: z.array(z.string()).optional(),
   color: z.string(),
   baseInteriorColor: z.string(),
   numberOfDoors: z.number(),
   price: z.number(),
-  tags: z.array(
-    z.object({ value: z.string(), label: z.string(), _id: z.string() })
-  ),
+  tags: z
+    .array(z.object({ value: z.string(), label: z.string(), _id: z.string() }))
+    .optional(),
+
   publishedAt: z.string(),
   createdAt: z.string(),
   updatedAt: z.string(),
-  slug: z.string(),
 });
 
 const carsDataSchema = z.array(carSchema);
