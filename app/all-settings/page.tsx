bd8050fb7ce76f504604f8baa62edcc043cef34a
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-11-28 09:08:55 +0600

    add: setting page content

--- app/all-settings/page.tsx
+++ app/all-settings/page.tsx
@@ -19,38 +19,36 @@ const SettingsPage = () => {
     setSelectedBtn(null);
   }
 
-  return <p>Settings Page</p>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-4 gap-5">
+      <div className="flex flex-col gap-5">
+        <Button
+          size={"4"}
+          onClick={() => handleClick("brand")}
+        >
+          Add Car Brand
+        </Button>
+        <Button
+          size={"4"}
+          onClick={() => handleClick("model")}
+        >
+          Add Car Model
+        </Button>
+        <Button
+          size={"4"}
+          onClick={() => handleClick("bodyType")}
+        >
+          Add Body Type
+        </Button>
+      </div>
 
-  // return (
-  //   <section className="grid grid-cols-1 lg:grid-cols-4 gap-5">
-  //     <div className="flex flex-col gap-5">
-  //       <Button
-  //         size={"4"}
-  //         onClick={() => handleClick("brand")}
-  //       >
-  //         Add Car Brand
-  //       </Button>
-  //       <Button
-  //         size={"4"}
-  //         onClick={() => handleClick("model")}
-  //       >
-  //         Add Car Model
-  //       </Button>
-  //       <Button
-  //         size={"4"}
-  //         onClick={() => handleClick("bodyType")}
-  //       >
-  //         Add Body Type
-  //       </Button>
-  //     </div>
-
-  //     <div className="col-span-3 lg:ml-15">
-  //       {selectedBtn == "brand" && <AddEditBrand formTitle="Add Brand" />}
-  //       {selectedBtn == "model" && <AddEditModel onClose={hideForm} />}
-  //       {selectedBtn == "bodyType" && <AddEditVehicleType onClose={hideForm} />}
-  //     </div>
-  //   </section>
-  // );
+      <div className="col-span-3 lg:ml-15">
+        {selectedBtn == "brand" && <AddEditBrand formTitle="Add Brand" />}
+        {selectedBtn == "model" && <AddEditModel onClose={hideForm} />}
+        {selectedBtn == "bodyType" && <AddEditVehicleType onClose={hideForm} />}
+      </div>
+    </section>
+  );
 };
 
 export default SettingsPage;
