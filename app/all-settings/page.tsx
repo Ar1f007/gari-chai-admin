498a230c9006f521110b9ccd883238e9744919c0
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-11-28 09:05:28 +0600

    remove: setting page content

--- app/all-settings/page.tsx
+++ app/all-settings/page.tsx
@@ -19,36 +19,38 @@ const SettingsPage = () => {
     setSelectedBtn(null);
   }
 
-  return (
-    <section className="grid grid-cols-1 lg:grid-cols-4 gap-5">
-      <div className="flex flex-col gap-5">
-        <Button
-          size={"4"}
-          onClick={() => handleClick("brand")}
-        >
-          Add Car Brand
-        </Button>
-        <Button
-          size={"4"}
-          onClick={() => handleClick("model")}
-        >
-          Add Car Model
-        </Button>
-        <Button
-          size={"4"}
-          onClick={() => handleClick("bodyType")}
-        >
-          Add Body Type
-        </Button>
-      </div>
+  return <p>Settings Page</p>;
 
-      <div className="col-span-3 lg:ml-15">
-        {selectedBtn == "brand" && <AddEditBrand formTitle="Add Brand" />}
-        {selectedBtn == "model" && <AddEditModel onClose={hideForm} />}
-        {selectedBtn == "bodyType" && <AddEditVehicleType onClose={hideForm} />}
-      </div>
-    </section>
-  );
+  // return (
+  //   <section className="grid grid-cols-1 lg:grid-cols-4 gap-5">
+  //     <div className="flex flex-col gap-5">
+  //       <Button
+  //         size={"4"}
+  //         onClick={() => handleClick("brand")}
+  //       >
+  //         Add Car Brand
+  //       </Button>
+  //       <Button
+  //         size={"4"}
+  //         onClick={() => handleClick("model")}
+  //       >
+  //         Add Car Model
+  //       </Button>
+  //       <Button
+  //         size={"4"}
+  //         onClick={() => handleClick("bodyType")}
+  //       >
+  //         Add Body Type
+  //       </Button>
+  //     </div>
+
+  //     <div className="col-span-3 lg:ml-15">
+  //       {selectedBtn == "brand" && <AddEditBrand formTitle="Add Brand" />}
+  //       {selectedBtn == "model" && <AddEditModel onClose={hideForm} />}
+  //       {selectedBtn == "bodyType" && <AddEditVehicleType onClose={hideForm} />}
+  //     </div>
+  //   </section>
+  // );
 };
 
 export default SettingsPage;
