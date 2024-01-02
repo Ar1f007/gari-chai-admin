1265b15587f90e1c70e43615f51199cea676b472
Ar1f007 <muhammadarif9416@yahoo.com>
2024-01-02 22:04:20 +0600

    add: handle videos

--- src/components/form/edit-new-car.tsx
+++ src/components/form/edit-new-car.tsx
@@ -27,6 +27,7 @@ import { toast } from "sonner";
 import { VideoUrl } from "../car/video-urls";
 
 const EditNewCarForm = ({ data }: { data: TCarSchema }) => {
+
   const brandData = brandSchema.parse(data.brand.value);
   const modelData = carModelSchema.parse(data.brandModel.value);
   const bodyStyle = carBodyStylesSchema.parse(data.bodyStyle.value);
@@ -41,17 +42,17 @@ const EditNewCarForm = ({ data }: { data: TCarSchema }) => {
 
       brand: {
         value: brandData._id,
-        label: brandData.name,
+        label: brandData.name
       },
 
       brandModel: {
         value: modelData._id,
-        label: modelData.name,
+        label: modelData.name
       },
 
       bodyStyle: {
         value: bodyStyle._id,
-        label: bodyStyle.name,
+        label: bodyStyle.name
       },
 
       tags: data.tags,
@@ -84,8 +85,8 @@ const EditNewCarForm = ({ data }: { data: TCarSchema }) => {
 
       status: data.status,
 
-      videos: data.videos,
-    },
+      videos: data.videos
+    }
   });
 
   const { uploadImage } = useUploadImage();
@@ -100,7 +101,7 @@ const EditNewCarForm = ({ data }: { data: TCarSchema }) => {
       ...data.imageUrls,
       data.posterImage.originalUrl,
       data.posterImage.thumbnailUrl,
-      ...colorImages(data.colors),
+      ...colorImages(data.colors)
     ];
 
     return imgUrls;
@@ -131,7 +132,7 @@ const EditNewCarForm = ({ data }: { data: TCarSchema }) => {
 
       return {
         originalUrl: res.url,
-        thumbnailUrl: res?.thumbnailUrl,
+        thumbnailUrl: res?.thumbnailUrl
       };
     } catch (error) {
       toast.error(
@@ -141,14 +142,54 @@ const EditNewCarForm = ({ data }: { data: TCarSchema }) => {
     }
   }
 
+  async function handleVideos(currentVideos: TCarSchema["videos"], prevVideos: TCarSchema["videos"]) {
+    if (!currentVideos.length) {
+      return prevVideos;
+    }
+
+    const videos: TCarSchema["videos"] = [];
+
+    for (const video of currentVideos) {
+
+      if (video.thumbnailImage && video.thumbnailImage instanceof File) {
+        try {
+          const res = await uploadImage(video.thumbnailImage);
+
+          if (!res) throw new Error(video.thumbnailImage.name);
+
+          video.thumbnailImage = {
+            originalUrl: res.url,
+            thumbnailUrl: res?.thumbnailUrl || res.url
+          };
+        } catch (e: any) {
+          if (e instanceof Error) {
+            return toast.error("Could not upload thumbnail image for video: " + e.message);
+          }
+          toast.error("Something went wrong while uploading thumbnail image for video");
+
+          return [];
+        }
+      } else {
+        videos.push({
+          link: video.link,
+          thumbnailImage: video.thumbnailImage
+        });
+      }
+    }
+
+    return videos;
+  }
+
   async function getFormattedPayload(formData: EditNewCarInputs) {
     const posterImage = !formData.posterImage
       ? data.posterImage
       : await getNewUrlAndDelPrevOne(formData.posterImage, data.posterImage.originalUrl);
 
+    // const videos = await handleVideos(formData.videos || [], data.videos);
+
     const payload = {
       ...formData,
-      posterImage: posterImage || data.posterImage,
+      posterImage: posterImage || data.posterImage
     };
 
     return payload;
