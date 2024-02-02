8a39ce4a65b6c71875000cfc7e787e219adf048b
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2024-02-02 13:07:00 +0600

    update: domain

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -15,7 +15,7 @@ export const API_V1_URL = API_BASE_URL + "/api/v1";
 
 export const UI_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://gari-chai.vercel.app"
+    ? "https://www.garichaibd.com"
     : "http://localhost:3000";
 
 export const PRIMARY_COLOR = "#3C50E0";
