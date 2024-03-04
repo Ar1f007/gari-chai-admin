2aac83ea46b543efc581f38af516671e359b56de
Ar1f007 <muhammadarif9416@yahoo.com>
2024-03-04 17:43:47 +0600

    add: display user info on table

--- src/components/users/users-table-column-def.tsx
+++ src/components/users/users-table-column-def.tsx
@@ -12,6 +12,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { DataTableColumnHeader } from "@/components/shared/data-table/data-table-column-header";
 import { DataTableRowActions } from "./users-table-row-actions";
 import { TAuthBasicUserInfo } from "@/schemas/user";
+import { cn, formatDate } from "@/lib/utils";
 
 export function fetchUsersTableColumnDefs(
   isPending: boolean,
@@ -23,8 +24,8 @@ export function fetchUsersTableColumnDefs(
       header: ({ table }) => (
         <Checkbox
           checked={
-            table.getIsAllPageRowsSelected()
-            // || (table.getIsSomePageRowsSelected() && "indeterminate")
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
           }
           onCheckedChange={(value) => {
             table.toggleAllPageRowsSelected(!!value);
@@ -46,22 +47,97 @@ export function fetchUsersTableColumnDefs(
       enableSorting: false,
       enableHiding: false,
     },
+    {
+      id: "firstName",
+      accessorKey: "firstName",
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title="First Name"
+        />
+      ),
+      cell: ({ row }) => (
+        <span className="capitalize font-medium">
+          {row.getValue("firstName")}
+        </span>
+      ),
+    },
+    {
+      id: "lastName",
+      accessorKey: "lastName",
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title="Last Name"
+        />
+      ),
+      cell: ({ row }) => (
+        <span className="capitalize font-medium">
+          {row.getValue("lastName")}
+        </span>
+      ),
+    },
+    {
+      id: "role",
+      accessorKey: "role",
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title="Roles"
+        />
+      ),
+      cell: ({ row }) => {
+        const roles = row.original.role;
+        const lastIndex = roles.length - 1;
 
-    // {
-    //   id: "name",
-    //   accessorKey: "name",
-    //   header: ({ column }) => (
-    //     <DataTableColumnHeader
-    //       column={column}
-    //       title="Name"
-    //     />
-    //   ),
-    //   cell: ({ row }) => (
-    //     <span className="capitalize truncate font-medium">
-    //       {row.getValue("name")}
-    //     </span>
-    //   ),
-    // },
+        return roles.map((role, idx) => (
+          <span
+            key={idx}
+            className={cn(
+              "capitalize font-medium mr-1",
+              idx === lastIndex && "mr-0"
+            )}
+          >
+            {role}
+            {idx == lastIndex ? null : ","}
+          </span>
+        ));
+      },
+      enableSorting: false,
+    },
+
+    {
+      id: "local",
+      accessorKey: "local",
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title="Email / Phone"
+        />
+      ),
+      cell: ({ row }) => {
+        const credential = row.original.local.email || row.original.local.phone;
+
+        return <span className="capitalize font-medium">{credential}</span>;
+      },
+      enableSorting: false,
+    },
+
+    {
+      id: "createdAt",
+      accessorKey: "createdAt",
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title="Joined"
+        />
+      ),
+      cell: ({ row }) => {
+        const date = formatDate(row.original.createdAt);
+
+        return <span className="capitalize font-medium">{date}</span>;
+      },
+    },
 
     {
       id: "actions",
@@ -72,28 +148,18 @@ export function fetchUsersTableColumnDefs(
 }
 
 export const filterableColumns: DataTableFilterableColumn<TAuthBasicUserInfo>[] =
-  [
-    // {
-    //   id: "launchedAt",
-    //   title: "Launch Date",
-    //   options: [
-    //     { label: "Launched Already", value: "past" },
-    //     { label: "Not Launched Yet", value: "future" },
-    //   ].map((status) => ({
-    //     label: status.label,
-    //     value: status.value,
-    //   })),
-    // },
-  ];
+  [];
 
 export const searchableColumns: DataTableSearchableColumn<TAuthBasicUserInfo>[] =
   [
-    // {
-    //   id: "name",
-    //   title: "Car Name",
-    // },
+    {
+      id: "firstName",
+      title: "First Name",
+    },
+    {
+      id: "lastName",
+      title: "Last Name",
+    },
   ];
 
-export const initialColumnVisibility = {
-  // launchedAt: true,
-};
+export const initialColumnVisibility = {};
